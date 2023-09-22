e2bc3b5b9c8bb529268bb24200e97f10aaab6470
Arslan Bulent <info.hecasolutions@gmail.com>
2023-09-22 11:20:10 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nuray seni cok seviyorum.
+          Askim seni cok seviyorum.
         </p>
         <a
           className="App-link"
