83aec58f89a60a7932d62e134b54ebe94707ca0d
Arslan Bulent <info.hecasolutions@gmail.com>
2023-09-22 11:15:43 +0200

    Test 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to nuray.
+          Nuray seni cok seviyorum.
         </p>
         <a
           className="App-link"
