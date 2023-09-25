564a9ea0d41880ff8031ebc601c99e864792ad49
Arslan Bulent <info.hecasolutions@gmail.com>
2023-09-25 09:43:43 +0200

    V6

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Sen benim herseyimsin.
+        Peter doe zo verder.
         </p>
         <a
           className="App-link"
