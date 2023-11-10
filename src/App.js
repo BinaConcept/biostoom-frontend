df901c16d56bd49aee246f445129a6b861f62a05
Arslan Bulent <info.hecasolutions@gmail.com>
2023-11-10 14:15:55 +0100

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Kurt Grosemans zo verder doen kerel.
+        Hakan en Michel niet geloven.
         </p>
         <a
           className="App-link"
