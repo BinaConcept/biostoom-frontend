896609e56ce107d0f648fb2e05720a2b7f19789a
Arslan Bulent <info.hecasolutions@gmail.com>
2023-11-10 16:26:47 +0100

    test

--- src/pages/start/StartPage.jsx
+++ src/pages/start/StartPage.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import bionerga_image from '../../assets/images/Bionerga_image.jpg';
-import {
-  BrowserRouter as 
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Link } from 'react-router-dom';
 const StartPage = () => {
 	return (
 		<div>
@@ -12,18 +9,13 @@ const StartPage = () => {
 				<div className="front_tin"></div>
 			</div>
 			<div className="glass_card text-center">
-				<div class="card text-center">
-					<div class="card-header">BIONERGA</div>
-					<div class="card-body">
-						<h5 class="card-title">WELKOM VERGUNNING PLATFORM</h5>
-						<p class="card-text">
-							Om werkvergunning aan te maken klik op start knop
-						</p>
-						<a href="#" class="btn btn-primary">
-							START
-						</a>
-					</div>
-					<div class="card-footer text-body-secondary">Year 2023@workpermits</div>
+				<h1>BIONERGA</h1>
+				<div>
+					<h6>WELKOM VERGUNNING PLATFORM</h6>
+					<p>Om werkvergunning aan te maken klik op start knop</p>
+					<a href="#" class="btn btn-primary">
+						START
+					</a>
 				</div>
 			</div>
 		</div>
