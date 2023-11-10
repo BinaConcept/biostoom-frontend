4474fae07826a8182ffd0f2dfdb5278d68160cd2
Arslan Bulent <info.hecasolutions@gmail.com>
2023-11-10 14:19:40 +0100

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hakan en Michel niet geloven.
+        Bulent en Kurt.
         </p>
         <a
           className="App-link"
