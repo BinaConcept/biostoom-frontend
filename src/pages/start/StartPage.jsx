cee93b0020f3299279dcb3643bf20c839bd99e5b
Arslan Bulent <info.hecasolutions@gmail.com>
2023-11-10 15:57:03 +0100

    laden

--- src/pages/start/StartPage.jsx
+++ src/pages/start/StartPage.jsx
@@ -10,9 +10,9 @@ const StartPage = () => {
 			<div className="glass_card text-center">
 				<h4>Welkom in Biostoom platform.</h4>
 				<p>Deze website dient om vergunningen aan te vragen</p>
-				<button type="button" className="btn btn-primary start_button">
+				{/* <button type="button" className="btn btn-primary start_button">
 					START
-				</button>
+				</button> */}
 				<div></div>
 			</div>
 		</div>
