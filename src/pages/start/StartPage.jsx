54793a62f16c9b6958e6538b0342bee37af054ba
Arslan Bulent <info.hecasolutions@gmail.com>
2023-11-10 16:20:20 +0100

    centrale card

--- src/pages/start/StartPage.jsx
+++ src/pages/start/StartPage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import bionerga_image from '../../assets/images/Bionerga_image.jpg';
+import {
+  BrowserRouter as 
+  Link
+} from "react-router-dom";
 const StartPage = () => {
 	return (
 		<div>
@@ -8,12 +12,19 @@ const StartPage = () => {
 				<div className="front_tin"></div>
 			</div>
 			<div className="glass_card text-center">
-				<h4>Welkom in Biostoom platform.</h4>
-				<p>Deze website dient om vergunningen aan te vragen</p>
-				{/* <button type="button" className="btn btn-primary start_button">
-					START
-				</button> */}
-				<div></div>
+				<div class="card text-center">
+					<div class="card-header">BIONERGA</div>
+					<div class="card-body">
+						<h5 class="card-title">WELKOM VERGUNNING PLATFORM</h5>
+						<p class="card-text">
+							Om werkvergunning aan te maken klik op start knop
+						</p>
+						<a href="#" class="btn btn-primary">
+							START
+						</a>
+					</div>
+					<div class="card-footer text-body-secondary">Year 2023@workpermits</div>
+				</div>
 			</div>
 		</div>
 	);
